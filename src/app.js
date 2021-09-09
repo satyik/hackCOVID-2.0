ae0150829e1a3382d9bfac9a87d5212c226ae2ac
satyik <55135748+satyik@users.noreply.github.com>
2021-09-10 00:46:46 +0530

    Added comments to the App.js file

--- src/app.js
+++ src/app.js
@@ -10,39 +10,54 @@ App = {
 
   loadAccount: async () => {
     const accounts = await ethereum.request({ method: 'eth_accounts' });
+    
+    // Set the current blockchain account
     App.account = accounts[0];
     console.log(accounts[0]);
   },
-
+  
+  
   loadContract: async () => {
+    // Create a JavaScript version of the smart contract
     const medProduct = await $.getJSON('MedProduct.json');
     App.contracts.MedProduct = TruffleContract(medProduct);
     App.contracts.MedProduct.setProvider(window.ethereum);
+    
+    // Hydrate the smart contract with values from the blockchain
     App.medProduct = await App.contracts.MedProduct.deployed();
   },
 
   render: async () => {
+    
+     // Prevent double render
     if (App.loading) {
       return;
     }
+    
+    //set app status loading
     App.setLoading(true);
+    // Render Account
     $('#account').html(App.account);
     console.log(App.account);
     App.setLoading(false);
   },
-
+  
+  // Javascript function to collect data from the create-medicine form and create a new block in the blockchain.
   createMed: async () => {
     App.setLoading(true);
     const medName = $('#medName').val();
     const mrp = $('#mrp').val();
     const expiry = $('#expiry').val();
     const manuDate = $('#manuDate').val();
-
+    
+    //using the newMed() function of smart contarct to create a new instance of the collected data.
     const medId = await App.medProduct.newMed(medName,mrp,expiry,manuDate, {from: App.account })
     console.log(medId);
     $('#medDetail').html(`The ID of your newely created medicine is: `+medId.logs[0].args[0].words[0]+"\n")
     console.log(medId.logs[0].args[0]);
   },
+  
+  // Javascript function to collect data from the create-retailer form and add the detail to the medicine block.
   addRet: async () => {
     App.setLoading(true);
     const mID = $('#mID').val();
@@ -54,6 +69,7 @@ App = {
     console.log(addRetailer);
   },
 
+  // Javascript function to display all details
   getMed: async () => {
     App.setLoading(true);
     const medicineID = $('#medicineID').val();
